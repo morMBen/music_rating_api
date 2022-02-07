13b3f44f1104f7defa3ee2ada774ccf836dea9b5
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 23:49:06 +0200

    restrictions

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 from .models import Song, Rating
 from .serializers import SongSerializer, RatingSerializer, UserSerializer
@@ -17,6 +18,7 @@ class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     authentication_classes = (TokenAuthentication,)
+    permission_classes = (AllowAny, )
 
     @action(detail=True, methods=['POST'])
     def rate_song(self, request, pk=None):
@@ -46,3 +48,12 @@ class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status.HTTP_400_BAD_REQUEST)
