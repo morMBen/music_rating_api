1ace34ac4a267423ecf04581140262b5168f7dd2
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 12:47:18 +0200

    Song model & Rating model

--- api/models.py
+++ api/models.py
@@ -1,2 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User  # import  the connected user
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+
+class Song(models.Model):
+    title = models.CharField(max_length=32)
+    artist = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+
+class Rating(models.Model):
+    # on_delete=models.CASCADE saying if we delete the song we delete the rating
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'song'),)
+        index_together = (('user', 'song'),)
