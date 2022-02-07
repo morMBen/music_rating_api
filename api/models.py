a4c1ec8ea9cec72a96b4e91c3d7188612ba61097
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 22:53:33 +0200

    add rating statistics functions to song model

--- api/models.py
+++ api/models.py
@@ -8,6 +8,21 @@ class Song(models.Model):
     artist = models.CharField(max_length=32)
     type = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(song=self)
+        return len(ratings)
+
+    def avg_ratings(self):
+        sum_of_ratings = 0
+        ratings = Rating.objects.filter(song=self)
+        for rating in ratings:
+            sum_of_ratings += rating.stars
+
+        if len(ratings) > 0:
+            return sum_of_ratings / len(ratings)
+        else:
+            return 0
+
 
 class Rating(models.Model):
     # on_delete=models.CASCADE saying if we delete the song we delete the rating
--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from .models import Song, Rating
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ('id', 'title', 'type', 'artist')
+        fields = ('id', 'title', 'type', 'artist', 'no_of_ratings', 'avg_ratings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
