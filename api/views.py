ed7e62709ae8c7a2f503fb6ac417f24ca1571c30
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 22:37:51 +0200

    update and create in custom methods

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Song, Rating
 from .serializers import SongSerializer, RatingSerializer
 
@@ -15,15 +16,27 @@ class SongViewSet(viewsets.ModelViewSet):
         if 'stars' in request.data:
 
             song = Song.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user # ← will be anonymous
+            user = User.objects.get(id=2)
 
-            response = {'song name': song.title}
-            return Response(response, status=status.HTTP_200_OK)
+            try:
+                rating = Rating.objects.get(user=user.id, song=song.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating Updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, song=song, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
         else:
             response = {'message': 'You need to provide stars'}
-            return Response(response, status= status.HTTP_400_BAD_REQUEST)
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-
