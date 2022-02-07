3210a8d2199527ae57747d2d882f74236cbaec22
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 21:04:44 +0200

    register viewSets to router

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import SongViewSet, RatingViewSet
+
 
 router = routers.DefaultRouter()
+router.register('songs', SongViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
