1d6a240c65ffbeedea49683ac5d84c3844313388
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-07 21:46:05 +0200

    adding custom methods

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Song, Rating
 from .serializers import SongSerializer, RatingSerializer
 
@@ -7,6 +9,11 @@ from .serializers import SongSerializer, RatingSerializer
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_song(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
